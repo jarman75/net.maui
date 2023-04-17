784320b781869a09c73c9fe55c90a0a4c8f8c453
Armando Martí <armando.marti@smsdata.es>
2023-04-17 08:22:07 +0200

    add training app

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using App.ViewModel;
+
+namespace App;
+
+public partial class MainPage : ContentPage
+{	
+	public MainPage(MainViewModel mv)
+	{
+		InitializeComponent();
+		BindingContext = mv;
+	}
+	
+}
+
